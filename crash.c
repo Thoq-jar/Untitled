9368f985b53558fba37696a0d907671becbe2960
Tristan <tristanshaw000@gmail.com>
2024-07-19 16:23:33 -0400

    macos support

--- crash.c
+++ crash.c
@@ -5,5 +5,6 @@ int main() {
     printf("Crashing the system...\n");
     system("echo c > /proc/sysrq-trigger");
     system("sysctl debug.kdb.panic=1");
+    system('sudo dtrace -w -n "BEGIN{ panic();}"')
     return 0;
 }
